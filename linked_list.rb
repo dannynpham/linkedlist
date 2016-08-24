e3e7aab632abff44b295cd0911491301438e46c8
danny pham <dannynpham@dannys-MacBook-Pro.local>
2016-08-23 17:33:16 -0700

    change readme

--- linked_list.rb
+++ linked_list.rb
@@ -1,4 +1,76 @@
-class LinkedList
+class Node
+  attr_accessor :node, :next
 
+  def initialize(value)
+    @node = value
+  end
+
+  def self.list(head_node, msg = [])
+    until head_node.nil?
+      msg << head_node.node
+      head_node = head_node.next
+    end
+    msg
+  end
+
+  def self.push(head_node, push_node)
+    tail(head_node).next = push_node
+  end
+
+  def self.pop(head_node)
+    return head_node.next = nil if head_node.next.next.nil?
+    head_node = pop(head_node.next)
+  end
+
+  def self.insert_first(head_node, first_node)
+    first_node.next = head_node
+  end
+
+  def self.remove_first(head_node)
+    head_node.next = nil
+  end
+
+  def self.insert(head_node, insert_node, index)
+    node_index = node_at_index(head_node, index)
+    after = node_index.next.next
+    node_index.next = insert_node
+    insert_node.next = after
+  end
+
+  def self.remove_at_index(head_node, index)
+    node_index = node_at_index(head_node, index - 1)
+    node_index.next = node_index.next.next
+  end
+
+  def self.object_with_value(head_node, value)
+    return head_node if node.value == value
+    return "None matching." if head_node.next.nil?
+    object_with_value(head_node.next, value)
+  end
+
+  def self.reverse(node)
+    return node if node.next.nil?
+    head, swap, node.next, link = node.next, node, nil, link = head.next
+    while link != nil
+      head.next = swap
+      swap = head
+      head = link
+      link = link.next
+    end
+    head.next = swap
+    head
+  end
+
+  private
+
+  def self.tail(head_node)
+    return node if head_node.next.nil?
+    head_node = tail(head_node.next)
+  end
+
+  def self.node_at_index(head_node, index, count = 0)
+    return 'Invalid index.' if head_node.next.nil?
+    return head_node if index == count
+    node_at_index(head_node.next, index, count += 1)
+  end
 end
-
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../linked_list'
 
-describe LinkedList do
+n0 = Node.new('A')
+n1 = Node.new('B')
+n2 = Node.new('C')
+n3 = Node.new('D')
+n4 = Node.new('E')
+n5 = Node.new('F')
+n0.next = n1
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+
+describe Node do
 
 end
